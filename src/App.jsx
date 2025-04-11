ed0498a459d48e298d732f11a470a0b82a747f50
VishalBasera <vishalbasera2000@gmail.com>
2025-04-11 15:33:29 +0530

    added tailwind configuration and other dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import "./App.css";
 
 function App() {
-  return <div className="font-mono bg-blue-200 rounded-xl">lms</div>;
+  return (
+    <div>
+      <div></div>
+      <p className="line-clamp-3">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec
+        fringilla libero Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Sed nec fringilla liberoLorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Sed nec fringilla liberoLorem ipsum dolor sit amet,
+        consectetur adipiscing elit. Sed nec fringilla liberoLorem ipsum dolor
+        sit amet, consectetur adipiscing elit. Sed nec fringilla liberoLorem
+        ipsum dolor sit amet, consectetur adipiscing elit. Sed nec fringilla
+        liberoLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec
+        fringilla liberoLorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Sed nec fringilla liberoLorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Sed nec fringilla liberoLorem ipsum dolor sit amet,
+        consectetur adipiscing elit. Sed nec fringilla libero Lorem ipsum dolor
+        sit amet, consectetur adipiscing elit. Sed nec fringilla liberoLorem
+        ipsum dolor sit amet, consectetur adipiscing elit. Sed nec fringilla
+        liberoLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec
+        fringilla liberoLorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Sed nec fringilla liberoLorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Sed nec fringilla libero
+      </p>
+    </div>
+  );
 }
 
 export default App;
